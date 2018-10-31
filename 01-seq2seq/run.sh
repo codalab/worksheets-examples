bf3f4c313fe88831781bb1a4417127b4d903ede0
Percy Liang <percyliang@gmail.com>
2018-10-31 09:12:06 -0700

    add translation pytorch tutorial

---
+++ 01-seq2seq/run.sh
@@ -0,0 +1,4 @@
+venv/bin/cl run :data :classification.py 'python classification.py'
+venv/bin/cl run :data :classification.py 'python classification.py' --request-gpus 1
+venv/bin/cl run :data :translation.py 'python translation.py'
+venv/bin/cl run :data :translation.py 'python translation.py' --request-gpus 1

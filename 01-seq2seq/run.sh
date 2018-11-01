189018d2847bd541d67925b43030ee94bcf5a770
Percy Liang <percyliang@gmail.com>
2018-10-31 20:13:20 -0700

    rename to SNLI, remove cruft

--- 01-seq2seq/run.sh
+++
@@ -1,4 +0,0 @@
-venv/bin/cl run :data :classification.py 'python classification.py'
-venv/bin/cl run :data :classification.py 'python classification.py' --request-gpus 1
-venv/bin/cl run :data :translation.py 'python translation.py'
-venv/bin/cl run :data :translation.py 'python translation.py' --request-gpus 1
